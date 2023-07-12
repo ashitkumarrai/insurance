9628c2ae2b7a9bcb23994c62e52aa9dfe1708d7a
Ashit Kumar Rai <68168801+ashitkumarrai@users.noreply.github.com>
2023-07-12 15:31:51 +0530

    config-done

--- src/main/java/com/serivce/insurance/config/CustomSecurityConfiguration.java
+++ src/main/java/com/serivce/insurance/config/CustomSecurityConfiguration.java
@@ -2,36 +2,17 @@ package com.serivce.insurance.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
-import org.springframework.security.ldap.authentication.BindAuthenticator;
-import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
-import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-
-import com.serivce.insurance.serviceimpl.LdapUserAuthoritiesPopulator;
-
-import jakarta.servlet.http.HttpServletResponse;
 
 
 @Configuration
 @EnableWebSecurity
 public class CustomSecurityConfiguration {
-
-    @Autowired
-    private Environment env;
-    
-    @Autowired
-    private LdapUserAuthoritiesPopulator ldapUserAuthoritiesPopulator;
 
  
        @Bean
